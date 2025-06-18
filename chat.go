b106743cb7eaa72d3bb75fa62ed585c9aa21616f
JP Meijers <jp.meijers@canonical.com>
2025-06-18 11:28:30 +0200

    Use readline library to allow special keys
    Check server on startup

--- chat.go
+++ chat.go
@@ -1,49 +1,109 @@
 package main
 
 import (
-	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/packages/ssestream"
+
+	"github.com/chzyer/readline"
 )
 
 func main() {
 	modelName := os.Getenv("MODEL_NAME")
 	reasoningModel := os.Getenv("REASONING_MODEL") == "True"
 
+	// OpenAI API Client
 	client := openai.NewClient()
-
-	param := openai.ChatCompletionNewParams{
+	params := openai.ChatCompletionNewParams{
 		Messages: []openai.ChatCompletionMessageParamUnion{},
 		Seed:     openai.Int(0),
 		Model:    modelName,
 	}
 
+	if err := checkServer(client, params); err != nil {
+		log.Fatalf("Connecting to server failed: %v", err)
+	}
+
 	fmt.Println("Type your prompt, then ENTER to submit. CTRL-C to quit.")
 
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt: "\033[31m»\033[0m ",
+		//HistoryFile:     "/tmp/readline.tmp",
+		//AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		//EOFPrompt:       "exit", // Does not work as expected
+
+		HistorySearchFold:   true,
+		FuncFilterInputRune: filterInput,
+	})
+	if err != nil {
+		log.Fatalf("Can't init readline: %v", err)
+	}
+	defer rl.Close()
+	//rl.CaptureExitSignal() // Should readline capture and handle the exit signal? - Can be used to interrupt the chat response stream.
+	log.SetOutput(rl.Stderr())
+
 	for {
-		fmt.Print("> ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		err := scanner.Err()
-		if err != nil {
-			log.Fatal(err)
+		prompt, err := rl.Readline()
+		if errors.Is(err, readline.ErrInterrupt) {
+			if len(prompt) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+		if prompt == "exit" {
+			break
 		}
 
-		param.Messages = append(param.Messages, openai.UserMessage(scanner.Text()))
-
-		stream := client.Chat.Completions.NewStreaming(context.Background(), param)
-		appendParam := processStream(stream, reasoningModel)
-
-		param.Messages = append(param.Messages, appendParam)
-		fmt.Println()
-		fmt.Println()
+		params = handlePrompt(client, params, reasoningModel, prompt)
 	}
+	fmt.Println("Closing chat")
+}
+
+func checkServer(client openai.Client, params openai.ChatCompletionNewParams) error {
+	params.Messages = []openai.ChatCompletionMessageParamUnion{openai.SystemMessage("You are a helpful assistant")}
+
+	timeoutContext, timeoutCancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer timeoutCancel()
+
+	_, err := client.Chat.Completions.New(timeoutContext, params)
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			// Connecting to server failed: context deadline exceeded
+			// Taking longer than the timeout means the LLM is thinking
+			return nil
+		} else {
+			// Post "http://server:8080/v3/chat/completions": dial tcp 192.168.86.81:8080: connect: connection refused
+			// POST "http://server:8080/v3/chat/completions": 404 Not Found "Mediapipe graph definition with requested name is not found"
+			return err
+		}
+	}
+	return nil
+}
+
+func handlePrompt(client openai.Client, params openai.ChatCompletionNewParams, reasoningModel bool, prompt string) openai.ChatCompletionNewParams {
+	params.Messages = append(params.Messages, openai.UserMessage(prompt))
+
+	stream := client.Chat.Completions.NewStreaming(context.Background(), params)
+	appendParam := processStream(stream, reasoningModel)
+
+	// Store previous prompts for context
+	params.Messages = append(params.Messages, appendParam)
+	fmt.Println()
+	fmt.Println()
+
+	return params
 }
 
 func processStream(stream *ssestream.Stream[openai.ChatCompletionChunk], printThinking bool) openai.ChatCompletionMessageParamUnion {
@@ -92,4 +152,13 @@ func processStream(stream *ssestream.Stream[openai.ChatCompletionChunk], printTh
 
 	// After the stream is finished, acc can be used like a ChatCompletion
 	return acc.Choices[0].Message.ToParam()
+}
+
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	// block CtrlZ feature
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
 }
